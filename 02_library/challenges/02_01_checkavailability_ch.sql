08dd190381c0fa49a7d125907e30d998485f4c33
Shashank Kumar <shashankys1234@gmail.com>
2022-11-11 20:13:18 +0000

    02_01, 02_02, 02_03

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,6 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+SELECT (SELECT COUNT(BookID) FROM Books where Title='Dracula')
+- (SELECT Count(BookID) From Loans where (BookID IN (SELECT BookID
+FROM Books where Title='Dracula') AND ReturnedDate is null))
+AS Available; 
--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,19 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+SELECT PatronID FROM Patrons where FirstName='Jack' AND 
+LastName='Vaan';
+
+SELECT BookID, Title, Barcode From Books where Title = 'The Picture of Dorian Gray'
+or Title = 'Great Expectations';
+
+SELECT DISTINCT(BookID) FROM Loans where BookID in (SELECT BookID From Books WHERE
+Title = 'The Picture of Dorian Gray' or Title = 'Great Expectations')
+AND ReturnedDate is not null ORDER BY BookID ASC Limit 1;
+
+INSERT INTO Loans(BookID, PatronID, LoanDate, DueDate, ReturnedDate)
+VALUES((SELECT BookID FROM Books where Barcode='2855934983'),(SELECT PatronID FROM Patrons where FirstName='Jack' AND 
+LastName='Vaan'),'2022-08-25','2022-09-08', null), ((SELECT BookID FROM Books where Barcode='4043822646'), 
+(SELECT PatronID FROM Patrons where FirstName='Jack' and LastName='Vaan'), '2022-08-25',
+'2022-09-08', null);
+SELECT * FROM Loans ORDER BY LoanID DESC LIMIT 2;
