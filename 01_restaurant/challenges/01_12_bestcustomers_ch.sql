5fafb36411462638b487af4d62ddbd74eff06194
Shashank Kumar <shashankys1234@gmail.com>
2022-11-11 19:23:08 +0000

    01_12

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,4 +1,9 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
-SELECT * FROM Customers;
+SELECT Customers.FirstName, Customers.LastName, Customers.Email,
+COUNT(OrderID)
+From Customers JOIN Orders ON Customers.CustomerID = 
+Orders.CustomerID GROUP BY 
+Orders.CustomerID ORDER BY COUNT(Orders.OrderID) DESC
+LIMIT 10;
